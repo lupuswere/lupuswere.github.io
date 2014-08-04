1c05460e87c85c053e1c348d7f2d494573425b00
lupuswere <lupuswere@gmail.com>
2014-08-04 10:41:45 -0700

    WeChatF

--- WeChatF/js/application.js
+++
@@ -1,6 +0,0 @@
-function generate() {
-    var restaurant = ['Chaping','Yummy Bites','China First','Meixin','Laogutai','Tofu House','Home'];
-    var index = parseInt(6*Math.random());
-    var name = restaurant[index];
-    document.getElementById("center").textContent = name;
-}
